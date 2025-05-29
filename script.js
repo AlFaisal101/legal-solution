7eba6c10987a2e0ced1926a777674af6c63eb174
AlFaisal101 <the.alfaisal1@gmail.com>
2025-05-29 13:46:46 +0600

    ___

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 
-        let items = document.querySelectorAll("#accordion .item .header");
+let items = document.querySelectorAll('#accordion .item .header');
 
-        items.forEach((item) => {
-            item.addEventListener('click',(e) => {
-                e.currentTarget.closest('.item').classList.toggle("active");
-                // alert("wefbd");
-            });
+items.forEach((item) => {
+    item.addEventListener('click', (e) => {
+
+        // remove active class from all items
+        items.forEach((header) => {      
+            header.closest('.item').classList.remove('active');
         });
-  
+        
+        // this change the clicked item
+        e.currentTarget.closest('.item').classList.toggle('active');
+    });
+});
